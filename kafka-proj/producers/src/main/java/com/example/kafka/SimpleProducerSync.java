9c7a4c2858d9011dc5bbd815441193e68a52da1c
Hyeonghwan <khhh9401@gmail.com>
2023-11-02 23:53:05 +0900

    consume sync'

---
+++ kafka-proj/producers/src/main/java/com/example/kafka/SimpleProducerSync.java
@@ -0,0 +1,47 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class SimpleProducerSync {
+    public static final Logger logger = LoggerFactory.getLogger(SimpleProducerSync.class.getName());
+    public static void main(String[] args) {
+        String topicName = "simple-topic";
+
+
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "id-001", "hello world2");
+
+        try {
+            Future<RecordMetadata> future = kafkaProducer.send(producerRecord);
+            RecordMetadata recordMetadata = future.get();
+            logger.info("received : " + recordMetadata.partition() + "//" + recordMetadata.offset());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            kafkaProducer.close();
+        }
+
+
+
+}
+
+
+}
