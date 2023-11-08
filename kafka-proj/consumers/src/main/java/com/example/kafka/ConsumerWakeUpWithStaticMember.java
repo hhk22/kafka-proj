d2818cc891b2fd93308f748f9ba5699cea983f03
Hyeonghwan <khhh9401@gmail.com>
2023-11-08 23:17:38 +0900

    static group member

---
+++ kafka-proj/consumers/src/main/java/com/example/kafka/ConsumerWakeUpWithStaticMember.java
@@ -0,0 +1,69 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerWakeUpWithStaticMember {
+
+    public static final Logger logger = LoggerFactory.getLogger(ConsumerWakeUpWithStaticMember.class.getName());
+
+    public static void main(String [] args) {
+
+        String topicName = "simple-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01_static");
+        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
+        kafkaConsumer.subscribe(List.of(topicName));
+
+        // main Thread
+        Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                logger.info("main program starts to exit by calling wakeup");
+                kafkaConsumer.wakeup();
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord record: consumerRecords) {
+                    logger.info("record key: {}, record value: {}, partition: {}",
+                            record.key(), record.value(), record.partition());
+                }
+            }
+        } catch (WakeupException e) {
+            logger.error("wakeup exception has been called");
+        } finally {
+            logger.info("finally consumer is closing");
+            kafkaConsumer.close();
+        }
+
+
+
+    }
+
+}
